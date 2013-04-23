2ab5e5f2b74096515dcc10427f2a7fc58f695862
gribbleshnibit8 <gribbleshnibit8@gmail.com>
2013-04-23 05:18:00 +0000

    Updated and fixed the Preset generation scripts to account for the bug with NVSE that will cause it to stop running on strings longer than 56 characters.

---
+++ Extra Options/Preset INIs/CreateSet.pl
@@ -0,0 +1,38 @@
+use strict;
+use warnings;
+
+system("cls");
+
+print "Please enter the name of the file: \n" ;
+my $file = <STDIN>;
+chop $file;
+
+# my $file = 'PN Extra Options.ini';
+open my $info, $file or die "Could not open $file: $!";
+open my $vars, "Variable Names.txt" or die "Could not open file";
+open (DATAFILE, "+>Set Scripts.txt");
+
+my @values;
+my $section;
+
+while( (my $line1 = <$info>) & (my $line2 = <$vars>))  {
+	print $line1;
+	next if $line1 =~ /^;/;
+	if ($line1 =~ /^\s/) {
+		print (DATAFILE "\n");
+		next;
+	}
+	if ($line1 =~ /^\[/) {
+		@values = split('\[', $line1);
+		$section = ($values[1]);
+		$section =~ tr/\$#@~!&*()[];.,:?^ `\\\///d;
+		chomp($section);
+		print (DATAFILE "\n\n; $section\n");
+		next;
+	}
+	chomp($line2);
+	@values = split('=', $line1);
+	print (DATAFILE "SetModINI \"PN Extra Options/$section/$values[0]\" $line2\n");
+}
+
+close $info;
